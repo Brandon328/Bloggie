61bd0dadc1d9924d98effdcf92346c41776d2807
DESKTOP-4EFGHJC\Brandon <bhuaman@solintels.com>
2025-05-12 20:16:31 -0500

    Se agrega el registro de usuarios

---
+++ Bloggie.Web/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using Bloggie.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<IdentityUser> _userManager;
+
+        public AccountController(UserManager<IdentityUser> userManager)
+        {
+            this._userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel request)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = request.Username,
+                Email = request.Email
+            };
+
+            var identityResult = await _userManager.CreateAsync(identityUser, request.Password);
+
+            if(identityResult.Succeeded)
+            {
+                // Assign thsi user the "User" role
+                var roleIdentityResult = await _userManager.AddToRoleAsync(identityUser, "User");
+
+                if(roleIdentityResult.Succeeded)
+                {
+                    // Show success notification
+                    return RedirectToAction("Register");
+                }
+            }
+
+            // Show error notification
+            return View();
+        }
+    }
+}
